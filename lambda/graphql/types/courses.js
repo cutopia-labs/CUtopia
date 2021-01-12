942d3c5c89d7b8efbe91ba928c59627e5b5a8254
Radionic <khpluslt@gmail.com>
2021-01-13 01:54:42 +0800

    Setup graphql server in Lambda

---
+++ lambda/graphql/types/courses.js
@@ -0,0 +1,41 @@
+module.exports = `
+  type Query {
+    subjects(filter: SubjectFilter): [Subject]!
+  }
+
+  type Subject {
+    name: String!
+    courses: [Course]!
+  }
+
+  input CourseFilter {
+    subject: [String]
+    maxNum: Int
+  }
+
+  input SubjectFilter {
+    requiredSubjects: [String]
+  }
+
+  type Course {
+    code: String!
+    title: String!
+    career: String!
+    units: String!
+    grading: String!
+    components: String!
+    campus: String!
+    academic_group: String!
+    requirements: String!
+    description: String!
+    outcome: String!
+    syllabus: String!
+    required_readings: String!
+    recommended_readings: String!
+  }
+
+  type AssessementComponent {
+    name: String!
+    percentage: String!
+  }
+`;
