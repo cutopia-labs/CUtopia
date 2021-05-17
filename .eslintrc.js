a9643e451dc723786cb49d9bbee86f9aec85a570
mikezzb <mikezhou96@gmail.com>
2021-05-17 16:49:27 +0800

    Added eslint and jsconfig

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+    browser: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  globals: {
+    React: true,
+    ReactDOM: true,
+    ReactRouterDOM: true,
+  },
+  plugins: [
+    'react',
+    'only-warn', // for run build
+  ],
+  rules: {
+    indent: ['error', 2, { SwitchCase: 1 }],
+    'no-console': 0,
+    'prettier/prettier': 0,
+    'react/prop-types': 0,
+    'react-hooks/exhaustive-deps': 0,
+    'react-native/no-inline-styles': 0,
+    'arrow-parens': ['error', 'as-needed'],
+    'brace-style': ['error', 'stroustrup'],
+    'object-curly-spacing': ['error', 'always'],
+    'linebreak-style': 0,
+    'react/jsx-filename-extension': [0],
+    'react/jsx-one-expression-per-line': ['error', { allow: 'single-child' }],
+  },
+  ignorePatterns: [
+    '*.css',
+  ],
+};
--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
+import React from 'react';
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+
+import ForumPage from './containers/forum';
 import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+const App = () => {
+  const preference = useContext(PreferenceContext);
+  const user = useContext(UserContext);
+  const client = useMemo(() => {
+    const httpLink = createHttpLink({
+      uri: 'https://7n5g73jlp6.execute-api.ap-northeast-1.amazonaws.com/Stage/graphql',
+    });
+    const authLink = setContext((_, { headers }) => {
+      const token = user.token;
+      return {
+        headers: {
+          ...headers,
+          authorization: token ? `Bearer ${token}` : '',
+        },
+      };
+    });
+    return new ApolloClient({
+      link: authLink.concat(httpLink),
+      cache: new InMemoryCache(),
+    });
+  }, [user.token]);
+
+  if (user.loginState !== undefined) {
+    console.log(user.loginState === LOGIN_STATES.LOGGED_IN_CUTOPIA ? 'Logged in' : 'Not Logged in');
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <HomePage
+              {...props}
+            />
+          </Route>
+          <Route exact path="/article/:id">
+            <ArticlePage {...props} />
+          </Route>
+          <Route exact path="/edit">
+            <EditPage {...props} />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
