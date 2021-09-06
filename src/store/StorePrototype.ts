3d707e9f8e17b4dc206bf3a47533373c5704620e
mikezzb <mikezhoudev@gmail.com>
2021-09-06 17:04:33 +0800

    Revamp store prototype

--- src/store/StorePrototype.ts
+++ src/store/StorePrototype.ts
@@ -1,16 +1,70 @@
 import { action } from 'mobx';
-
-import { storeData } from '../helpers/store';
+import { getStoreData, removeStoreItem, storeData } from '../helpers/store';
 
 class StorePrototype {
+  onLoadKeys?: string[];
+  onResetKeys?: string[];
+  defaultValues?: Record<string, any>;
+
+  constructor(
+    onLoadKeys?: string[],
+    onResetKeys?: string[],
+    defaultValues?: Record<string, any>
+  ) {
+    this.onLoadKeys = onLoadKeys;
+    this.onResetKeys = onResetKeys;
+    this.defaultValues = defaultValues;
+  }
+
   @action.bound updateStore(key: string, value: any) {
     this[key] = value;
   }
 
-  @action async setStore(key: string, value: any) {
+  @action loadStore = async () => {
+    const defaultValues = { ...this.defaultValues };
+    if (this.onLoadKeys) {
+      await Promise.all(
+        this.onLoadKeys.map(async key => {
+          const retrieved = await getStoreData(key);
+          this.updateStore(
+            key,
+            retrieved === null
+              ? (this.defaultValues || {})[key] || retrieved
+              : retrieved
+          );
+          delete defaultValues[key];
+        })
+      );
+    }
+    this.initStore(defaultValues);
+  };
+
+  @action setStore = async (key: string, value: any) => {
     this.updateStore(key, value);
-    storeData(key, value);
-  }
+    await storeData(key, value);
+  };
+
+  @action resetStore = async () => {
+    const defaultValues = { ...this.defaultValues };
+    if (this.onResetKeys) {
+      await Promise.all(
+        this.onResetKeys.map(async key => {
+          await removeStoreItem(key);
+          this.updateStore(key, (this.defaultValues || {})[key] || null);
+          delete defaultValues[key];
+        })
+      );
+    }
+    this.initStore(defaultValues);
+  };
+
+  @action initStore = (defaultValues?: Record<string, any>) => {
+    if (this.defaultValues) {
+      Object.entries(defaultValues || this.defaultValues).forEach(([k, v]) => {
+        this.updateStore(k, v);
+      });
+    }
+  };
 }
 
 export default StorePrototype;
