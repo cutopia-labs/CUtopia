24851bb8c7b77e8b39dd803b10417f80322b7f69
Radionic <khpluslt@gmail.com>
2021-09-07 23:06:23 +0800

    Update email template

--- lambda/emailer/template.js
+++ lambda/emailer/template.js
@@ -12,14 +12,16 @@ exports.createUserTemplate = ({ SID, username, code }) => ({
   from: 'CUtopia <cutopia.app@gmail.com>',
   to: `${SID}@link.cuhk.edu.hk`,
   subject: 'CUtopia confirmation email',
-  text: `Thanks for using CUtopia! Please click the following link to verify:
-https://cutopia.app/account/verify?user=${username}&code=${code}`,
+  html: `Thanks for using CUtopia! To verify your account, please click 
+<a href='https://cutopia.app/account/verify?user=${username}&code=${code}'>here</a> OR 
+manually enter the verification code: ${code} in the website or mobile app.`,
 });
 
 exports.resetPwdTemplate = ({ SID, username, code }) => ({
   from: 'CUtopia <cutopia.app@gmail.com>',
   to: `${SID}@link.cuhk.edu.hk`,
   subject: 'CUtopia reset password',
-  text: `Please click the following link to reset your password:
-http://cutopia.app/account/reset-password?user=${username}&code=${code}`,
+  html: `To reset your password, please click 
+<a href='http://cutopia.app/account/reset-password?user=${username}&code=${code}'>here</a> OR 
+manually enter the code: ${code} in the website or mobile app.`,
 });
