f14b94e4a42f0fe84d835d3ba157ff8b5b3561aa
Radionic <khpluslt@gmail.com>
2021-09-14 20:16:15 +0800

    Fix background processes not killed on exit & build gql when modified

--- local-compile.sh
+++ local-compile.sh
@@ -1,6 +1,8 @@
 yarn --cwd mongodb/ run build
 yarn --cwd lambda/graphql run build
 
+trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
+
 tsc --project lambda/graphql -w & \
   nodemon --watch "mongodb/src" -e ts --exec "tsc --project mongodb/ && yarn --cwd lambda/graphql/ run upgrade-db" & \
-  nodemon --watch "lambda/graphql/src/schemas/*.graphql" -e graphql --exec "cd lambda/graphql && sh copyfile.sh" -q & \
+  nodemon --watch "lambda/graphql/src/schemas/*.graphql" -e graphql --ignore "lambda/graphql/src/schemas/bundle.graphql" --exec "yarn --cwd lambda/graphql/ run local-gql"
