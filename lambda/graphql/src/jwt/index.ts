4a73a2b5e2fb0fd44bbc3525f381953c84463489
Radionic <khpluslt@gmail.com>
2021-11-12 02:22:43 +0800

    Refresh token when expired

--- lambda/graphql/src/jwt/index.ts
+++ lambda/graphql/src/jwt/index.ts
@@ -1,35 +1,52 @@
-import jwt from 'jsonwebtoken';
+import jwt, { JwtPayload } from 'jsonwebtoken';
 import fs from 'fs';
 import { join } from 'path';
 import { getUser } from 'mongodb';
 import { ErrorCode } from 'cutopia-types/lib/codes';
+import { ApolloError } from 'apollo-server-errors';
 
 const privateKey = fs.readFileSync(join(__dirname, './jwtRS256.key'));
 const publicKey = fs.readFileSync(join(__dirname, './jwtRS256.key.pub'));
 
-export interface Token {
+export interface Token extends JwtPayload {
   username: string;
   password: string;
 }
 
 export const sign = (payload: Token) => {
-  const token = jwt.sign(payload, privateKey, { algorithm: 'RS256' });
+  const token = jwt.sign(payload, privateKey, {
+    algorithm: 'RS256',
+    expiresIn: '7d',
+  });
   return token;
 };
 
 export const verify = async token => {
   try {
-    var decoded = jwt.verify(token, publicKey) as Token;
+    var decoded = jwt.verify(token, publicKey, {
+      ignoreExpiration: true,
+    }) as Token;
   } catch (e) {
     return null;
   }
-  const { username, password } = decoded;
-  const user = await getUser(
-    { username },
-    cachedUser => cachedUser.password !== password
-  );
-  if (user?.password !== password) {
-    throw Error(ErrorCode.AUTHORIZATION_INVALID_TOKEN.toString());
+
+  if (Date.now() >= decoded.exp * 1000) {
+    const { username, password } = decoded;
+    const user = await getUser({ username });
+    if (user?.password !== password) {
+      throw new ApolloError(
+        'Invalid token',
+        ErrorCode.AUTHORIZATION_INVALID_TOKEN.toString()
+      );
+    } else {
+      throw new ApolloError(
+        'Expired token',
+        ErrorCode.AUTHORIZATION_REFRESH_TOKEN.toString(),
+        {
+          refreshedToken: sign({ username, password }),
+        }
+      );
+    }
   }
   return decoded;
 };
