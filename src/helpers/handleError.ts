744203f0837c3d6970f26d3683dae2ef22087a87
mikezzb <mikezhoudev@gmail.com>
2022-02-24 21:41:03 +0800

    Fix refresh token bug

--- src/helpers/handleError.ts
+++ src/helpers/handleError.ts
@@ -1,3 +1,5 @@
+// @ts-nocheck
+
 import { ApolloError } from '@apollo/client';
 import { ErrorCode } from 'cutopia-types/lib/codes';
 import { ERROR_MESSAGES } from '../constants/errors';
@@ -6,10 +8,19 @@ import ViewStore from '../store/ViewStore';
 
 const handleError = (e: ApolloError, view: ViewStore) => {
   const err_code = parseInt(e.message, 10);
+  const customErrors = isNaN(err_code) ? e.networkError?.result?.errors : null;
   // Clear the invalid token and log out
-  if (err_code === ErrorCode.AUTHORIZATION_INVALID_TOKEN) {
+  if (
+    customErrors?.length &&
+    parseInt(customErrors[0]?.extensions?.code, 10) ===
+      ErrorCode.AUTHORIZATION_INVALID_TOKEN
+  ) {
     console.log('Invalid token, logging out!');
     userStore.logout();
+    const refreshedToken = customErrors[0]?.extensions?.refreshedToken;
+    if (refreshedToken) {
+      userStore.saveToken(refreshedToken);
+    }
   }
   view.setSnackBar({
     message: ERROR_MESSAGES[err_code] || e.message,
