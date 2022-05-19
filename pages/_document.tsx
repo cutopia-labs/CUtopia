a5cd8f588301af04fbc65a631b32097b343d3a24
mikezzb <mikezhoudev@gmail.com>
2022-05-19 13:15:03 +0800

    Add common document

---
+++ pages/_document.tsx
@@ -0,0 +1,31 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+import { FC } from 'react';
+
+const Document: FC = () => (
+  <Html>
+    <Head>
+      <link rel="icon" href="%PUBLIC_URL%/cutopia-logo.png" />
+      <title>CUtopia</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="theme-color" content="#000000" />
+      <meta
+        name="description"
+        content="CUtopia is a course review and planning platform for CUHK students."
+      />
+      <link rel="apple-touch-icon" href="%PUBLIC_URL%/cutopia-logo.png" />
+      <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link
+        href={`https://fonts.googleapis.com/css2?family=Pacifico&display=swap`}
+        rel="stylesheet"
+      />
+    </Head>
+    <body>
+      <Main />
+      <NextScript />
+    </body>
+  </Html>
+);
+
+export default Document;
