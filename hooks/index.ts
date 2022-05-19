dca1201ede3814f61b2a4aa85f03a9aaf3da2505
mikezzb <mikezhoudev@gmail.com>
2022-05-19 17:17:20 +0800

    Chore: delete unused pages

--- hooks/index.ts
+++
@@ -1,6 +0,0 @@
-import { useContext } from 'react';
-import { PlannerContext, UserContext, ViewContext } from '../store';
-
-export const useUser = () => useContext(UserContext);
-export const usePlanner = () => useContext(PlannerContext);
-export const useView = () => useContext(ViewContext);
