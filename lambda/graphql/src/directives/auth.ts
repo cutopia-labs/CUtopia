a30393024a1641583c71496c4b9b384e1d22e4a4
Radionic <khpluslt@gmail.com>
2022-05-28 17:35:52 +0800

    Chore: refactor directives

--- lambda/graphql/src/directives/auth.ts
+++ lambda/graphql/src/directives/auth.ts
@@ -3,29 +3,22 @@ import { mapSchema, getDirective, MapperKind } from '@graphql-tools/utils';
 import { ErrorCode } from 'cutopia-types/lib/codes';
 
 // reference: https://www.graphql-tools.com/docs/schema-directives#enforcing-access-permissions
+const authDirectiveTypeDefs = `directive @auth on OBJECT | FIELD_DEFINITION`;
+const authDirectiveTransformer = (schema: GraphQLSchema) =>
+  mapSchema(schema, {
+    [MapperKind.OBJECT_FIELD]: (fieldConfig, _fieldName, typeName) => {
+      const authDirective = getDirective(schema, fieldConfig, 'auth')?.[0];
+      if (authDirective) {
+        const { resolve = defaultFieldResolver } = fieldConfig;
+        fieldConfig.resolve = (parent, args, context, info) => {
+          if (!context.user) {
+            throw Error(ErrorCode.AUTHORIZATION_INVALID_TOKEN.toString());
+          }
+          return resolve(parent, args, context, info);
+        };
+        return fieldConfig;
+      }
+    },
+  });
 
-const authDirective = (directiveName: string) => ({
-  authDirectiveTypeDefs: `directive @${directiveName} on OBJECT | FIELD_DEFINITION`,
-  authDirectiveTransformer: (schema: GraphQLSchema) =>
-    mapSchema(schema, {
-      [MapperKind.OBJECT_FIELD]: (fieldConfig, _fieldName, typeName) => {
-        const authDirective = getDirective(
-          schema,
-          fieldConfig,
-          directiveName
-        )?.[0];
-        if (authDirective) {
-          const { resolve = defaultFieldResolver } = fieldConfig;
-          fieldConfig.resolve = (parent, args, context, info) => {
-            if (!context.user) {
-              throw Error(ErrorCode.AUTHORIZATION_INVALID_TOKEN.toString());
-            }
-            return resolve(parent, args, context, info);
-          };
-          return fieldConfig;
-        }
-      },
-    }),
-});
-
-export default authDirective;
+export { authDirectiveTypeDefs, authDirectiveTransformer };
--- lambda/graphql/src/directives/index.ts
+++ lambda/graphql/src/directives/index.ts
@@ -1,26 +1,12 @@
-import authDirective from './auth';
 import {
   constraintDirective,
   constraintDirectiveTypeDefs,
 } from 'graphql-constraint-directive';
+import { authDirectiveTypeDefs, authDirectiveTransformer } from './auth';
 import {
-  defaultKeyGenerator,
-  rateLimitDirective,
-} from 'graphql-rate-limit-directive';
-import { ErrorCode } from 'cutopia-types/lib/codes';
-import { Context } from '../context';
-
-const { authDirectiveTypeDefs, authDirectiveTransformer } =
-  authDirective('auth');
-// reference: https://github.com/ravangen/graphql-rate-limit/tree/master/examples/context
-const { rateLimitDirectiveTypeDefs, rateLimitDirectiveTransformer } =
-  rateLimitDirective({
-    keyGenerator: (...params) =>
-      `${(params[3] as Context).ip}:${defaultKeyGenerator(...params)}`,
-    onLimit: () => {
-      throw new Error(ErrorCode.EXCEED_RATE_LIMIT.toString());
-    },
-  });
+  rateLimitDirectiveTypeDefs,
+  rateLimitDirectiveTransformer,
+} from './rateLimit';
 
 const directivesTypeDefs = [
   authDirectiveTypeDefs,
