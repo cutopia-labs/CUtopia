039447ed528bee632f12fe98c6378beade01c743
mikezzb <mikezhoudev@gmail.com>
2022-07-16 23:51:27 +0800

    Chore: update folder structure

---
+++ tools/coursesLoader.ts
@@ -0,0 +1,45 @@
+import fs from 'fs';
+import { Course } from 'cutopia-types/lib/types';
+import getConfig from 'next/config';
+const { serverRuntimeConfig } = getConfig();
+
+export interface CourseExtended extends Course {
+  courseId: string;
+}
+
+const ignoredFiles = new Set(['.DS_Store']);
+
+const courses: Record<string, CourseExtended> = {};
+
+const dirname = `${process.cwd()}/data/courses`;
+
+const subjectFilenames = fs.readdirSync(dirname);
+
+console.log(`Loading ${subjectFilenames.length} subjects in ${dirname}`);
+
+subjectFilenames.forEach(subjectFileName => {
+  if (ignoredFiles.has(subjectFileName)) return;
+  const filepath = fs.readFileSync(`${dirname}/${subjectFileName}`).toString();
+  const courseList: Course[] = JSON.parse(filepath);
+  if (courseList.length !== 0) {
+    const subjectName = subjectFileName.split('.')[0];
+    courseList
+      .filter(course => course)
+      .forEach(course => {
+        const courseId = `${subjectName}${course.code}`;
+        const assessmentsNames = Object.keys(course?.assessments || {});
+        courses[courseId] = {
+          ...course,
+          assessments: assessmentsNames.map(name => ({
+            name,
+            percentage: course.assessments[name],
+          })),
+          courseId,
+        };
+      });
+  }
+});
+
+console.log(`Loaded e.g. AIST: ${courses['AIST3010'].title}`);
+
+export { courses };
