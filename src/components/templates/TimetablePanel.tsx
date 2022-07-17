f0305a8435c3be679ee030e15efc54d37144c553
mikezzb <mikezhoudev@gmail.com>
2022-07-17 14:46:30 +0800

    Chore: cleanup

--- src/components/templates/TimetablePanel.tsx
+++ src/components/templates/TimetablePanel.tsx
@@ -1,14 +1,5 @@
-import { useState, FC } from 'react';
-import {
-  Button,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogContentText,
-  DialogTitle,
-  TextField,
-  IconButton,
-} from '@material-ui/core';
+import { FC } from 'react';
+import { IconButton } from '@material-ui/core';
 import { observer } from 'mobx-react-lite';
 
 import copy from 'copy-to-clipboard';
@@ -23,12 +14,6 @@ import TimetableOverview, {
   TimetableOverviewProps,
 } from '../planner/TimetableOverview';
 
-enum MODAL_MODES {
-  NO_MODAL,
-  IMPORT_MODAL,
-  EXPORT_MODAL,
-}
-
 type TimetablePanelProps = {
   onShare?: (...args: any[]) => any;
   className?: string;
@@ -42,15 +27,10 @@ const TimetablePanel: FC<TimetablePanelProps> = ({
 }) => {
   const view = useView();
   const planner = usePlanner();
-  const [modalMode, setModalMode] = useState(MODAL_MODES.NO_MODAL);
-  const [importInput, setImportInput] = useState('');
   const courses = planner.plannerCourses
     ?.concat(planner.previewPlannerCourse)
     .filter(course => course);
   const timetableInfo = planner.timetableInfo;
-  const onImport = parsedData =>
-    planner.updateStore('plannerCourses', parsedData);
-
   const onClear = () => planner.clearPlannerCourses();
 
   const FUNCTION_BUTTONS = [
@@ -99,51 +79,6 @@ const TimetablePanel: FC<TimetablePanelProps> = ({
         courses={(courses?.slice() || []) as any}
         timetableInfo={timetableInfo}
       />
-      <Dialog
-        open={modalMode === MODAL_MODES.IMPORT_MODAL}
-        onClose={() => setModalMode(MODAL_MODES.NO_MODAL)}
-        aria-labelledby="form-dialog-title"
-      >
-        <DialogTitle id="form-dialog-title">Import</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            Paste the shared json string here!
-            {/*
-            1. Visit <a href="https://cusis.cuhk.edu.hk/psc/CSPRD/EMPLOYEE/SA/c/SSR_STUDENT_FL.SSR_COMPONENT_FL.GBL">CUSIS Timetable Page</a>
-            2. Right click and select View Page Source
-            3. Copy n paste to here
-            */}
-          </DialogContentText>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            label="JSON"
-            type="text"
-            fullWidth
-            onChange={e => setImportInput(e.target.value)}
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button
-            onClick={() => setModalMode(MODAL_MODES.NO_MODAL)}
-            color="primary"
-          >
-            Cancel
-          </Button>
-          <Button
-            color="primary"
-            onClick={() => {
-              setModalMode(MODAL_MODES.NO_MODAL);
-              if (importInput) {
-                onImport(JSON.parse(importInput));
-              }
-            }}
-          >
-            Import
-          </Button>
-        </DialogActions>
-      </Dialog>
     </Card>
   );
 };
