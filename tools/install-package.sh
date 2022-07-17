d85a9aa3a4723ea9a05a0bcb0a621d9a4cd5f9ee
Radionic <khpluslt@gmail.com>
2022-07-17 18:06:03 +0800

    Chore: fix install script order

--- tools/install-package.sh
+++ tools/install-package.sh
@@ -1,21 +1,28 @@
 isProd=${NODE_ENV:-development}
 
-for d in $(find . -type f -name 'package.json' | grep -v "node_modules" | sed -r 's|/[^/]+$||'); do
-  # If the dir is nested in built directory, continue
-  if [[ $d = */lib/* ]] || [[ $d = */build/* ]]; then
-    # echo "Skipped output dir in $d"
-    continue
-  fi
+declare -a modules=(
+  "./"
+  "./mongodb"
+  "./lambda/emailer"
+  "./lambda/graphql"
+  "./lambda/cron-remove-timetable"
+  "./lambda/cron-update-ranking"
+  "./tools/load-test"
+)
+
+for d in "${modules[@]}"; do
   if [ -d "$d/node_modules" ]; then
     echo "Removing node_modules in $d"
     rm -rf $d/node_modules
   fi
-  echo "Installing package in $d"
+
+  echo "Installing node_modules in $d"
   if [ $isProd = "production" ]; then
     yarn --cwd $d install --prod
   else
     yarn --cwd $d install
   fi
+
   # Build the mongodb for lambda to install it
   if [ $d = "./mongodb" ]; then
     echo "Building mongodb"
