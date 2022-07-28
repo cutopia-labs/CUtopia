7c5e93f94555c7b2387334e9f7bc765f9969c106
mikezzb <mikezhoudev@gmail.com>
2022-07-29 00:13:50 +0800

    Chore: setup gulp

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+const gulp = require('gulp');
+const stripDebug = require('gulp-strip-debug');
+
+gulp.task(
+  'strip-debug',
+  () =>
+    gulp
+      .src('[.src/**.ts, ./src/**.tsx]') // input file path
+      .pipe(stripDebug()) // execute gulp-strip-debug
+      .pipe(gulp.dest('.src/')) // output file path
+);
