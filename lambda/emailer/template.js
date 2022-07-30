57c184fc44656950cd9c472dbb1a8baf273b38d3
Radionic <khpluslt@gmail.com>
2022-07-31 00:51:01 +0800

    Update email templates

--- lambda/emailer/template.js
+++ lambda/emailer/template.js
@@ -12,9 +12,9 @@ exports.createUserTemplate = ({ SID, username, code }) => ({
   from: 'CUtopia <cutopia.app@gmail.com>',
   to: `${SID}@link.cuhk.edu.hk`,
   subject: 'CUtopia confirmation email',
-  html: `Thanks for using CUtopia! To verify your account, please click 
-<a href='https://cutopia.app/account/verify?user=${username}&code=${code}'>here</a> OR 
-manually enter the verification code: ${code} in the website or mobile app.`,
+  html: `Thanks for using CUtopia!<br /><br /> To verify your account, please click 
+  <a href='https://cutopia.app/login?mode=verify&username=${username}&code=${code}'>here</a> OR 
+manually enter the verification code: <b>${code}</b><br /><br />Regards,<br />CUtopia Team`,
 });
 
 exports.resetPwdTemplate = ({ SID, username, code }) => ({
@@ -22,6 +22,6 @@ exports.resetPwdTemplate = ({ SID, username, code }) => ({
   to: `${SID}@link.cuhk.edu.hk`,
   subject: 'CUtopia reset password',
   html: `To reset your password, please click 
-<a href='http://cutopia.app/account/reset-password?user=${username}&code=${code}'>here</a> OR 
-manually enter the code: ${code} in the website or mobile app.`,
+<a href='https://cutopia.app/login?mode=reset-pwd&username=${username}&code=${code}'>here</a> OR 
+manually enter the code: <b>${code}</b><br /><br />Regards,<br />CUtopia Team`,
 });
