cafe6c018a47493cc3a8e41519f91e6a4a1cc78e
mikezzb <mikezhoudev@gmail.com>
2022-07-31 02:11:01 +0800

    Chore: update server id

--- src/config.ts
+++ src/config.ts
@@ -32,7 +32,7 @@ const AWS_REGION = 'ap-southeast-1';
  * mike-dev: uvp03fp791
  */
 export const SERVER_ID =
-  process.env.NODE_ENV === 'production' ? 'd8r6qarg78' : 'd8r6qarg78';
+  process.env.NODE_ENV === 'production' ? 'f6le32w9fj' : 'f6le32w9fj';
 
 export const SERVER_ADDR = `https://${SERVER_ID}.execute-api.${AWS_REGION}.amazonaws.com/Prod`;
 
