b0b6b42bb8d5a661293cf963156f0a9881445f33
mikezzb <mikezhoudev@gmail.com>
2022-07-30 23:30:58 +0800

    Chore: fix sentry config

--- sentry.client.configs.js
+++ sentry.client.configs.js
@@ -4,7 +4,7 @@ import { Integrations } from '@sentry/tracing';
 // use sentry to catch exceptions
 Sentry.init({
   dsn:
-    process.env.NODE_ENV !== 'production'
+    process.env.NODE_ENV === 'production'
       ? 'https://c38359448a5448a58971eeb211568473@o861810.ingest.sentry.io/5821571'
       : '',
   tracesSampleRate: 0.3,
