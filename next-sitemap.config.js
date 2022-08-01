62dfeb23d96dd1073c08bb88ddd36433d094ee27
mikezzb <mikezhoudev@gmail.com>
2022-08-01 13:50:28 +0800

    Chore: fix site url

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 const config = {
-  siteUrl: process.env.SITE_URL || 'cutopia.app',
+  siteUrl: process.env.SITE_URL || 'https://cutopia.app',
   generateRobotsTxt: true, // (optional)
   outDir: 'build',
   // ...other options
