712ae9c92c54d22c657ab8f5644eb7f8539205df
mikezzb <mikezhoudev@gmail.com>
2022-08-22 18:18:34 +0800

    Chore: cleanup

--- src/components/atoms/If.tsx
+++ src/components/atoms/If.tsx
@@ -10,11 +10,4 @@ const If: FC<IfProps> = ({ visible, children, elseNode = null }) => {
   return visible ? children : elseNode;
 };
 
-/*
-export default memo(
-  If,
-  (props, oldProps) => props.visible === oldProps.visible
-);
-*/
-
 export default If;
