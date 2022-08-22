5f17732b7dd6da1216c434cca1f91c9adf2c6adb
Radionic <khpluslt@gmail.com>
2022-08-23 01:39:51 +0800

    Chore: skip setting passphrase

--- tools/init.sh
+++ tools/init.sh
@@ -48,12 +48,11 @@ AUTH_PASSWORD=password of a CUtopia account\
 # Step 5: generate RSA key pair
 # Reference: https://gist.github.com/ygotthilf/baa58da5c3dd1f69fae9
 
-mkdir "lambda/graphql/src/jwt"
+mkdir -p "lambda/graphql/src/jwt"
 publicKeyPath="lambda/graphql/src/jwt/jwtRS256.key.pub"
 privateKeyPath="lambda/graphql/src/jwt/jwtRS256.key"
 
-ssh-keygen -t rsa -b 4096 -m PEM -f $privateKeyPath
-# Don't add passphrase
+ssh-keygen -t rsa -b 4096 -m PEM -f $privateKeyPath -N ""
 openssl rsa -in $privateKeyPath -pubout -outform PEM -out $publicKeyPath
 cat $publicKeyPath
 cat $privateKeyPath
