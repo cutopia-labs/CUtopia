baf08e9f101dc38ae3211d21c6d93ea34ffa573e
Radionic <khpluslt@gmail.com>
2022-08-25 22:55:13 +0800

    Retrieve partial course data in Lambda

--- tools/init.sh
+++ tools/init.sh
@@ -1,5 +1,5 @@
-# Step 1: install git submodules
-git submodule update --init
+# Step 1: fetch course data from remote repository
+sh tools/update-data.sh
 
 # Step 2: install node modules
 sh tools/install-package.sh
