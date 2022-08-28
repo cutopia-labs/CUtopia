b1dae9f8180857e9c247c10c9e154277cf02c7ec
mikezzb <mikezhoudev@gmail.com>
2022-08-28 16:47:28 +0800

    Fix hex opacity

--- frontend/src/helpers/updateOpacity.ts
+++ frontend/src/helpers/updateOpacity.ts
@@ -1,9 +1,10 @@
 const RGB_REPLACE_RULE = new RegExp('[\\d\\.]+\\)$', 'g');
 
+const addHexZero = (str: string) => (str.length === 1 ? `0${str}` : str);
 const addOpacity = (rgbString: string, opacity: number) => {
   if (rgbString.startsWith('#')) {
     const hexOpacity = Math.round(Math.min(Math.max(opacity || 1, 0), 1) * 255);
-    return rgbString + hexOpacity.toString(16).toUpperCase();
+    return rgbString + addHexZero(hexOpacity.toString(16).toUpperCase());
   }
   if (rgbString.startsWith('rgba')) {
     return rgbString.replace(RGB_REPLACE_RULE, `${opacity})` as any);
