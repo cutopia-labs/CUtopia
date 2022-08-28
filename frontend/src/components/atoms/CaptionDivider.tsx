9d270d8ceeb2e81e6402365dd11249de21c44a36
mikezzb <mikezhoudev@gmail.com>
2022-08-28 15:23:05 +0800

    Add caption divider

---
+++ frontend/src/components/atoms/CaptionDivider.tsx
@@ -0,0 +1,17 @@
+import { Divider } from '@mui/material';
+import clsx from 'clsx';
+import { FCC } from '../../types/general';
+import styles from '../../styles/components/atoms/CaptionDivider.module.scss';
+
+type Props = {
+  className: string;
+};
+
+const CaptionDivider: FCC<Props> = ({ children, className }) => (
+  <span className={clsx('row', styles.captionDivider, className)}>
+    {children}
+    <Divider />
+  </span>
+);
+
+export default CaptionDivider;
