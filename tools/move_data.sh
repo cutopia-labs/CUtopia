e89c917b53ed38a285d142517ee524c80d25fd01
mikezzb <mikezhoudev@gmail.com>
2022-08-29 20:54:02 +0800

    Auto commit data update

---
+++ tools/move_data.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Move data
+readonly DATA=$(dirname `pwd`)/data
+readonly TOOL=$(dirname `pwd`)/tools
+echo moving $1 to $DATA
+cp -r $TOOL/data/$1/* $DATA
+cd ../data
+# Commit data
+set -x
+git add .
+git commit -am "Update data @$1"
+git push origin master
