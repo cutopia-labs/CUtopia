a3f9e08461116eec6a8fb5990c2bcbb7fde49bd9
mikezzb <mikezhoudev@gmail.com>
2022-08-31 16:51:32 +0800

    Chore: update events type

--- frontend/src/types/events.ts
+++ frontend/src/types/events.ts
@@ -1,37 +1,5 @@
-import { CourseSection, PlannerCourse } from './courses';
+import { CourseTableEntry, PlannerCourse } from 'cutopia-types';
 import { TimetableOverviewMode } from './enums';
-
-export interface Event {
-  courseId: string;
-  day: number;
-  startTime: string;
-  endTime: string;
-  color?: string;
-  title?: string;
-  location?: string;
-  section?: string;
-}
-
-export interface Events {
-  startTimes: string[];
-  endTimes: string[];
-  days: number[];
-  locations: string[];
-}
-
-export type EventConfig = {
-  startHour: number;
-  endHour: number;
-  numOfDays: number;
-  numOfHours: number;
-};
-
-export type CourseTableEntry = {
-  courseId: string;
-  title: string;
-  credits: number;
-  sections?: CourseSection[];
-};
 
 export type UploadTimetableResponse = {
   id: string;
