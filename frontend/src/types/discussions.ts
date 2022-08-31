35e932784f742448d443e48ded961a3a67f84360
mikezzb <mikezhoudev@gmail.com>
2022-08-31 16:47:12 +0800

    Chore: update discussion type

--- frontend/src/types/discussions.ts
+++ frontend/src/types/discussions.ts
@@ -1,18 +1,5 @@
-export type DiscussionMessage = {
+import { DiscussionMessage as DiscussionMessageRaw } from 'cutopia-types';
+
+export type DiscussionMessage = DiscussionMessageRaw & {
   id?: number;
-  _id?: number;
-  text: string;
-  user: string;
-};
-
-export type Discussion = {
-  _id: string;
-  messages: [DiscussionMessage];
-};
-
-export type DiscussionRecent = {
-  courseId: string;
-  text: string;
-  time?: number;
-  user: string;
 };
