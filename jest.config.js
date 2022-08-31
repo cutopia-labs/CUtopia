2c419b5843bb317ee8cfa281bbc58b085e9b4611
Radionic <khpluslt@gmail.com>
2022-08-31 18:14:44 +0800

    Chore: refactor jest config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  modulePathIgnorePatterns: ['<rootDir>/lib/', '<rootDir>/node_modules/'],
+  modulePathIgnorePatterns: ['types/lib/', 'backend/mongodb/lib/'],
 };
