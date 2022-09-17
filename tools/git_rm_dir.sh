053a3bdce36e15466fd901e6b2ea92c3e897a355
mikezzb <mikezhoudev@gmail.com>
2022-09-17 14:33:41 +0800

    Chore: remove unused scripts

--- tools/git_rm_dir.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-git filter-branch --tree-filter "rm -rf $1" --prune-empty HEAD
-git for-each-ref --format="%(refname)" refs/original/ | xargs -n 1 git update-ref -d
-echo $1/ >> .gitignore
-git add .gitignore
-git commit -m "Removing $1 from git history"
-git gc
